9de93afa4921c1d0bef53abe126fe909c494ea4b
Navnish Singh <navnish.singh@synergymachines.us>
2022-04-14 00:09:36 +0530

    Add header for dashboard

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,6 +5,10 @@ import {AppHeader} from "./AppHeader"
 export const Header = () => {
     const location = useLocation();
 
+    if(location.pathname.includes("/login")){
+        return null;
+    }
+
     if (location.pathname === "/"){
         return <LandingHeader />
     }
